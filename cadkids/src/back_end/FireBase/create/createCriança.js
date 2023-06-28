dfbaab15350351864791a9e17900560128236515
José Eduardo Nogueira <eduardosoaresn3@gmail.com>
2023-06-28 20:59:27 -0300

    Teste

--- cadkids/src/back_end/FireBase/create/createCriança.js
+++ cadkids/src/back_end/FireBase/create/createCriança.js
@@ -1,8 +1,42 @@
 import { DB }                             from 'src/firebaseConfig';
-import { addDoc  ,collection }                     from 'firebase/firestore';
+import { doc, addDoc  ,collection, setDoc }                     from 'firebase/firestore';
 export function createCrianca(data,navigation){
-    addDoc(
-        collection(DB,'Criança'),data
+    
+//    data = data + collection(DB, 'vacinas')
+
+    let vaccines = [
+        {
+            Nome: "BCG 1 dose",
+            Descricao: "Previne contra poliomelite",
+            Idade: 0
+        },
+        {
+            Nome: "Febre Amarela",
+            Descricao: "Previne contra febre amarela",
+            Idade: 12
+        }
+    
+    ]
+
+    const postRef = collection("Criança", DB)
+    .doc(auth.currentUser.Email)
+    .collection("vacinas", DB);
+
+    for(let i in vaccines){
+        setDoc(doc(DB, postRef, i.Nome), i)
+    }
+    
+    // addDoc(
+    //     collection(DB,'Criança'),data
+    // ).then(()=> {
+    //     navigation.navigate('Inicio_crianca',{Email:data.Email});
+    // }
+    // ).catch((error)=>{ 
+    //     alert('Invalido'+error.message);
+    // });
+
+    setDoc(
+        doc(DB, "Criança", data.Email), data
     ).then(()=> {
         navigation.navigate('Inicio_crianca',{Email:data.Email});
     }
